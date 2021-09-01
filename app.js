dab82a1d34d8b4d3d469e82deff6ad0a246e8240
M. Isa Gul <isagul7890@gmail.com>
2021-09-01 17:12:47 +0300

    ejs added, navigation fixed

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require('express');
-
 const app = express();
+const path = require('path');
 
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+//MIDDLEWARES
+app.use(express.static('public'));
+
+//ROUTES
 app.get('/', (req, res) => {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-  res.send(blog);
+  res.render('index');
 });
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+
+
+
+
 
 const port = 3000;
 app.listen(port, () => {

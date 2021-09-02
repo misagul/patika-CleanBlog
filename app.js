dd763ae8f496683472cb06d36c8a914091bd8dad
M. Isa Gul <isagul7890@gmail.com>
2021-09-02 21:37:27 +0300

    Databese created, schame added, post form fixed

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+const ejs = require('ejs');
 const path = require('path');
+
+const Post = require('./models/Post');
+const app = express();
+
+//CONNECT DATABASE
+mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const posts = await Post.find({})
+  res.render('index',{
+    posts
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -21,9 +36,10 @@ app.get('/add_post', (req, res) => {
   res.render('add_post');
 });
 
-
-
-
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body)
+  res.redirect('/');
+});
 
 const port = 3000;
 app.listen(port, () => {
